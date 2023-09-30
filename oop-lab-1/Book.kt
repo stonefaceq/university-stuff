e3d140c1ff63c0d5d1d2dba8f832c44733f6ad63
Andrew Dubyna <andrewdubyna9@gmail.com>
2023-09-30 16:37:47 +0300

    minor fix

--- oop-lab-1/Book.kt
+++ oop-lab-1/Book.kt
@@ -2,7 +2,7 @@ import java.lang.IllegalArgumentException
 //class used to store info about books, characters, year of creation, author.
 abstract class Book(val name : String, val year: String, val author: String) {
     open lateinit var canonicity : CanonicEnums
-    val characters = mutableMapOf<String, RoleEnums>("John Doe" to RoleEnums.MAIN, "Jane Doe" to RoleEnums.SECONDARY, "Bob" to RoleEnums.EPISODIC)// here we can access character's role by their name
+    val characters = mutableMapOf<String, RoleEnums>()// here we can access character's role by their name
 
     fun createCharacter(name: String, role: String) {
         role.lowercase()
@@ -26,9 +26,7 @@ abstract class Book(val name : String, val year: String, val author: String) {
         }
     }
 
-    fun createBookGraph()  {
-        val Bgraph1 = Graph<Book>()
-    }
+
     abstract fun showBookData(name: String)
 
     override fun toString(): String {
